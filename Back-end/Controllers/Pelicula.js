4d306b820859fee50e3bc1ef9ba690b3fed5d5d7
Saurix39 <saurix39@gmail.com>
2021-10-15 22:47:58 -0500

    prueba2

--- Back-end/Controllers/Pelicula.js
+++ Back-end/Controllers/Pelicula.js
@@ -78,7 +78,7 @@ var peliculaController={
                 }
                 var file_path = req.files.file0.path;
                 console.log(file_path);
-                var file_split = file_path.split('\\');
+                var file_split = file_path.split('\/');
                 var file_name = file_split[2];
                 var ext_split = file_name.split('\.');
                 var file_ext = ext_split[1];
